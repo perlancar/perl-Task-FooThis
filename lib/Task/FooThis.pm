611e41343ed699d92fbfc340637887dbdf7899fa
perlancar (on netbook-dell-vostro) <perlancar@gmail.com>
2017-11-10 09:58:10 +0700

    Initial commit

---
+++ lib/Task/FooThis.pm
@@ -0,0 +1,13 @@
+package Task::FooThis;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Export your directory over various channels
+
+=pkggroup Included modules
+
+#CODE: for (`acme-cpanlists ls-entries "Export your directory over various channels"`) { print "=pkg $_\n" }
+
+=cut
